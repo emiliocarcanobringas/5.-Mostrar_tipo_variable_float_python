5c465b3e3a99dfde939b70e43f65240bf9f5e1a6
Emilio Carcaño Bringas <emiliocarcano@uninaciones.edu.mx>
2020-12-03 19:40:04 -0600

    Add files via upload

---
+++ 5.-Mostrar_tipo_variable_float_python.py
@@ -0,0 +1,9 @@
+#Este programa muestra como imprimir un valor en pantalla y mostrar que tipo de variable es.
+
+print("Este programa muestra como imprimir un valor en pantalla y mostrar de qué tipo es: ")
+numero = 3.1416
+print("El valor de la variable es: ")
+print(type(numero))
+print("Este programa fue escrito por Emilio Carcaño Bringas")
+
+#Este programa fue escrito por Emilio Carcaño Bringas
